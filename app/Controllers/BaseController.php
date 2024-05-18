a3478c05e295011ad3a9a321c562e7b6035278c3
Jose Bohorquez <bd567358546@gmail.com>
2024-05-18 02:42:05 -0500

    solucione error No input file specified editando el htaccess de la ruta public

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -42,7 +42,6 @@ abstract class BaseController extends Controller
      * The creation of dynamic property is deprecated in PHP 8.2.
      */
     // protected $session;
-       protected $session;
 
     /**
      * @return void
@@ -55,6 +54,5 @@ abstract class BaseController extends Controller
         // Preload any models, libraries, etc, here.
 
         // E.g.: $this->session = \Config\Services::session();
-                 $this->session = \Config\Services::session();
     }
 }
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,13 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        #return view('welcome_message');
+        return "ruta automatica";
+    }
+
+    public function mira(): string
+    {
+        #return view('welcome_message');
+        return "ruta xxx";
     }
 }
