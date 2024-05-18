5d00c87119ffd533f820fa240ebd98f9108e06be
Jose Bohorquez <bd567358546@gmail.com>
2024-05-18 08:37:40 -0500

    creamos view layout con bootstrapWatch

--- app/Controllers/Landing.php
+++ app/Controllers/Landing.php
@@ -6,11 +6,14 @@ class Landing extends BaseController
 {
     public function index(): string
     {
-        return view('landing/modulos/1_header')
-        . view('landing/modulos/2_btnTema')
-        . view('landing/modulos/3_navbarSup')
-        . view('landing/paginas/inicio')
-        . view('landing/modulos/4_footer');
+        // return view('landing/modulos/1_header')
+        // . view('landing/modulos/2_btnTema')
+        // . view('landing/modulos/3_navbarSup')
+        // . view('landing/paginas/inicio')
+        // . view('landing/modulos/4_footer');
+
+        $datosPagina = ["titulo" => "inicio"];
+        return view('landing/2_landingViewLayouts/inicio', $datosPagina);
     }
 
     public function mira(): string
---
+++ app/Views/landing/2_landingViewLayouts/inicio.php
@@ -0,0 +1,9 @@
+<?php echo $this->extend("landing/2_landingViewLayouts/layout"); ?>
+
+<?php echo $this->section("contenido"); ?>
+<h1 class="text-center">
+    bienvenid@s a ServiTech_BD
+</h1>
+<p class="text-center">Estamos para atenderlos 24/7</p>
+<p class="text-center"> <a href="https://youtu.be/AMMWxabCBZI?list=PL-Mlm_HYjCo9od4jKwVimQN7P3dK07xAb&t=527" target="__banck">Quedamos aca</a></p>
+<?php echo $this->endSection(); ?>
